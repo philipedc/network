0f2689cb5297d2983e324c45186018c9cc2111f6
philipedc <philipedutra2002@gmail.com>
2023-09-24 19:07:19 -0300

    minefield logic added to both client and server

--- TP1/client.c
+++ TP1/client.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "minefield.h"
 
 #include <stdio.h> 
 #include <unistd.h>
@@ -41,18 +42,47 @@ int main(int argc, char* argv[]){
     printf("Connected to %s\n", addrstr);
 
     struct action buf[BUFFSIZE];
+    memset(buf, 0, BUFFSIZE+1);
     while(1){
-        memset(buf, 0, BUFFSIZE);
-        scanf("%d", &buf->type);
-        if (buf->type == 7) break;
-        printf("Data sent: ");
-        
+        char input[20];
+        char* command;
+        char* str_coord;
+        fgets(input, 20, stdin);
+        command = strtok(input, " ");
+        command[strcspn(command, "\n")] = 0; //TRIMMING
+        str_coord = strtok(NULL, " ");
+        if (str_coord != NULL){
+            buf->coordinates[0] = atoi(strtok(str_coord, ","));
+            buf->coordinates[1] = atoi(strtok(NULL, ","));
+        }
+
+        // printf("<%s %d %d>\n", command, buf->coordinates[0], buf->coordinates[1]);
+
+        if (strcmp(command, "start") == 0) buf->type = 0;
+        else if (strcmp(command, "reveal") == 0) buf->type = 1;
+        else if (strcmp(command, "flag") == 0) buf->type = 2;
+        else if(strcmp(command, "remove_flag") == 0) buf->type = 4;
+        else if(strcmp(command, "reset") == 0) buf->type = 5;
+        else if(strcmp(command, "game_over") == 0) buf->type = 8;
+        else if(strcmp(command, "exit") == 0) break;
+        else buf->type = -1;
+        // printf("Data sent: {type: %d, coordinates: [%d, %d]}", buf->type, buf->coordinates[0], buf->coordinates[1]);
+        if (check_errors(buf->type, buf->coordinates, buf->board) != 0) continue;
+
         size_t count = send(s, buf, BUFFSIZE+1, 0);
         if (count != BUFFSIZE+1) logexit("send");
 
         memset(buf, 0, BUFFSIZE);
         if (recv (s, buf, BUFFSIZE+1, 0) == -1) logexit("recv");
-        printf("Data received: %d\n", buf->type);
+
+        if (buf->type == 6){
+            printf("YOU WIN!\n");
+        }
+
+        if (check_bomb(buf->board, buf->coordinates) == true){
+            printf("GAME OVER!\n");
+        }
+        print_game(buf->board);
     }
     close(s);
     exit(EXIT_SUCCESS);
--- TP1/server.c
+++ TP1/server.c
@@ -6,9 +6,11 @@
 #include <sys/socket.h>
 #include <sys/types.h> 
 
+#include "minefield.h"
 #include "common.h"
 
 #define PENDING_CONNECTIONS 10
+#define SIZE 4
 
 struct action{
     int type;
@@ -16,32 +18,55 @@ struct action{
     int board[4][4];
 };
 
+int update_matrix(int matrix[SIZE][SIZE], struct action *buf){
+    for (int i = 0; i < SIZE; i++){
+        for (int y = 0; y < SIZE; y++){
+            buf->board[i][y] = matrix[i][y];
+        }
+    }
+    return 0;
+}
+
+int play_game(char* path, int board[SIZE][SIZE], int revealed[SIZE][SIZE], struct action *buf){
+
+    perform_action(path, buf->type, buf->coordinates, board, revealed);
+    if (check_win(revealed) == 1){
+        printf("You won!\n");
+        buf->type = 6;
+    }
+    update_matrix(revealed, buf);
+    return 0;
+}
+
 int main(int argc, char* argv[]){
     const int BUFFSIZE = sizeof(struct action);
 
-    // if (argc != 5){
-    //     printf("Usage: <v4/v6> <Port Number> -i <Path>\n");
-    //     exit(EXIT_FAILURE);
-    // }
-    char *input_path;
-    int opt;
-    // while((opt = getopt(argc, argv, "i:")) != -1) 
-    // { 
-    //     switch(opt) 
-    //     { 
-    //         case 'i':
-    //             input_path = optarg;
-    //             break;
+    if (argc != 5){
+        printf("Usage: <v4/v6> <Port Number> -i <Path>\n");
+        exit(EXIT_FAILURE);
+    }
 
-    //         default:
-    //             printf("Usage: <v4/v6> <Port Number> -i <Path>\n");
-    //             exit(EXIT_FAILURE);
-    //     }
-    // }
-    // printf("Input path: %s\n", input_path);
+    char* address_family = argv[1];
+    char* port = argv[2];
+
+    char *path = "input.txt";
+    int opt;
+    while((opt = getopt(argc, argv, "i:")) != -1) 
+    { 
+        switch(opt) 
+        { 
+            case 'i':
+                path = optarg;
+                break;
+
+            default:
+                printf("Usage: <v4/v6> <Port Number> -i <Path>\n");
+                exit(EXIT_FAILURE);
+        }
+    }
 
     struct sockaddr_storage storage;
-    if (server_sockaddr_init(argv[1], argv[2], &storage) != 0){
+    if (server_sockaddr_init(address_family, port, &storage) != 0){
         printf("Usage: <v4/v6> <Port Number> -i <Path>\n");
     }
 
@@ -59,7 +84,7 @@ int main(int argc, char* argv[]){
     addrtostr(addr, addrstr, BUFFSIZE);
     printf("Bound to %s, waiting connections\n", addrstr);
 
-    while(1){
+    while(1){ //Listening
         struct sockaddr_storage *cstorage;
         struct sockaddr *caddr = (struct sockaddr *) &cstorage;
         socklen_t caddrlen = sizeof(cstorage);
@@ -72,11 +97,15 @@ int main(int argc, char* argv[]){
         printf("[log] connection from %s\n", caddrstr);
 
         struct action buf[BUFFSIZE];
-        while(1){
+        while(1){ // Reading input from client
             memset(buf, 0, BUFFSIZE);
             size_t count = recv(csock, buf, BUFFSIZE+1, 0);
-            printf("[MSG] %s, %d bytes: %d\n", caddrstr, (int)count, buf->type);
-            // sprintf(buf, "Remote endpoint: %.1000s\n", caddrstr);
+
+            int board[SIZE][SIZE];
+            int revealed[SIZE][SIZE];
+            play_game(path, board, revealed, buf);
+
+            printf("[MSG] %s, %d bytes {command: %d, coordinates: %d %d}\n", caddrstr, (int)count, buf->type, buf->coordinates[0], buf->coordinates[1]);
 
             count = send(csock, buf, BUFFSIZE+1, 0);
             if (count != BUFFSIZE+1) logexit("send");
